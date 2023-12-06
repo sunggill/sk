07d233c3c74d0915cbb6b6574902725f93691f17
sunggill <88535740+sunggill@users.noreply.github.com>
2023-12-06 19:23:01 +0900

    Update 01_summarized_text.py

--- 01_summarized_text.py
+++ 01_summarized_text.py
@@ -1,43 +1,41 @@
 import streamlit as st
 import openai
 
-# 챗지피티에게 글 요약을 요청하는 함수
-def askGPT(prompt, apikey):
-    client = openai.OpenAI(api_key=apikey)
+# 챗지피티에게 글요약을 요청하는 함수
+def askGPT(prompt,apiKey):
+    client = openai.OpenAI(api_key=apiKey)
     response = client.chat.completions.create(
-        model = 'gpt-3.5-turbo',
+        model='gpt-3.5-turbo',
         messages=[
-            {"role":"user","content":promt}
-        ]
+            {"role":"user","content":prompt}
+        ]        
     )
-
     finalResponse = response.choices[0].message.content
     return finalResponse
 
 ## main 함수
 def main():
-    st.set_page_config(page_tile="요약 프로그램")
+    st.set_page_config(page_title="요약 프로그램")
 
     # session_state 초기화
     if "OPENAI_API" not in st.session_state:
         st.session_state["OPENAI_API"] = ""
-
+    
     with st.sidebar:
-        open_apikey = st.text_input(label='OPEN API 키', placeholder='Enter your api key')
-
-        if open_apikey:
-            st.session_state["OPENAI_API"] = open_apikey
-        st.markdown('----')
-
+        open_apiKey = st.text_input(label='OPEN API 키',placeholder='Enter your api key')
+        
+        if open_apiKey:
+            st.session_state["OPENAI_API"] = open_apiKey
+        st.markdown('---')
 
     st.header(":scroll:요약 프로그램:scroll:")
-    st.markdown('------')
+    st.markdown('---')
 
     text = st.text_area("요약 할 글을 입력하세요")
-    if st.button("요약"):        
+    if st.button("요약"):
         prompt = f'''
-        **Instructions*
-          assistant that summarizes text into **Korean language**.
+        **Instructions** :
+    - You are an expert assistant that summarizes text into **Korean language**.
     - Your task is to summarize the **text** sentences in **Korean language**.
     - Your summaries should include the following :
         - Omit duplicate content, but increase the summary weight of duplicate content.
@@ -46,4 +44,7 @@ def main():
         - Use the format of a bullet point.
     -text : {text}
     '''
+        st.info(askGPT(prompt,st.session_state["OPENAI_API"]))
 
+if __name__ == "__main__":
+    main()
